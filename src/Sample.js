3679d0e405bbf80ed6dee1e3db28ec7b4cb67fd2
Decima <decima567@gmail.com>
2022-11-06 13:01:15 +0530

    Fonts and icons

---
+++ src/Sample.js
@@ -0,0 +1,30 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+
+import {SafeAreaView, StyleSheet, StatusBar, Text} from 'react-native';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+const Sample = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <MaterialIcons name="home" size={30} color="#900" />
+      <Ionicons name="pizza" size={30} color="#900" />
+      <Text style={{fontFamily: 'Roboto-Bold', fontSize: 20}}>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    marginTop: StatusBar.currentHeight || 0,
+
+    justifyContent: 'center',
+
+    alignItems: 'center',
+  },
+});
+
+export default Sample;
