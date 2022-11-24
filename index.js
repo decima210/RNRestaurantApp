e605f8ab5bdb0d589920047437530a42268bf353
Decima <decima567@gmail.com>
2022-11-24 18:55:19 +0530

    Files for Restaurant app

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
-
+//import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
-import Sample from './src/Sample';
 
-AppRegistry.registerComponent(appName, () => Sample);
+AppRegistry.registerComponent(appName, () => App);
---
+++ src/common/CommonStyles.js
@@ -0,0 +1,13 @@
+import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+export default {
+  width,
+  height,
+
+  colors: {
+    //base coloors
+    primary: '#FE724C', //orange
+    secondary: '#252828', //black
+  },
+};
